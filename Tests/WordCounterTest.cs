22341f3fcb9c6cfaeec97c0399af9a31b0025802
David Wilson <davidtheadmiral@gmail.com>
2017-06-02 09:22:32 -0700

    build test method to check if the user input is a sentence

--- Tests/WordCounterTest.cs
+++ Tests/WordCounterTest.cs
@@ -6,11 +6,18 @@ namespace WordCounter
   public class WordCounterTest
   {
     [Fact]
-    public void Is_CheckForWordEntered_True()
+    public void Is_CheckForWordEntered_true()
     {
       string userInput = "Bird";
       RepeatCounter newSingleWord = new RepeatCounter();
       Assert.Equal(true, newSingleWord.CheckForWord(userInput));
+    }
+    [Fact]
+    public void Is_CheckForSentenceEntered_true()
+    {
+      string userInput = "Bird, bird, bird, bird is the word";
+      RepeatCounter newSentence = new RepeatCounter();
+      Assert.Equal(true, newSentence.CheckForSentence(userInput));
     }
   }
 }
